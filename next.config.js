14e765a96755e6fe547783c0150cb64c3602d9a8
Yuriy Boev <yuriy.boev@osf-commerce.com>
2022-04-15 22:51:43 +0300

    Added i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate')
+
 const nextConfig = {
+  i18n: {
+    locales: ['en', 'de'],
+    defaultLocale: 'en',
+    // localesToIgnore: ['default'],
+  },
+  trailingSlash: false,
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextTranslate(nextConfig)
